bc9f2c3241ab3cde8664797ec899c1e08b765c0c
Daniel <39141263+uaineteine@users.noreply.github.com>
2024-10-20 01:34:46 +1100

    use swifter

--- hash_ds.py
+++ hash_ds.py
@@ -1,18 +1,19 @@
 import pandas as pd
 import hmac
 import hashlib
+import swifter
 
-def hash_and_truncate_hmac(df, column, key, length=24):
-    def hmac_and_truncate_value(value, key, length):
-        hashed_value = hmac.new(key.encode(), str(value).encode(), hashlib.sha256).hexdigest()
-        return hashed_value[:length]
+def hmac_and_truncate_value(value, key, length=24):
+    hashed_value = hmac.new(key.encode(), str(value).encode(), hashlib.sha256).hexdigest()
+    return hashed_value[:length]
 
+def hash_and_truncate_hmac_swifter(df, column, key, length=24):
     new_col_name = column + "_hash"
-    df[new_col_name] = df[column].apply(lambda x: hmac_and_truncate_value(x, key, length))
+    df[new_col_name] = df[column].swifter.apply(lambda x: hmac_and_truncate_value(x, key, length))
     return df
 
 # Example usage
 data = {'usernames': ['user1', 'user2', 'user3']}
 df = pd.DataFrame(data)
-hashed_df = hash_and_truncate_hmac(df, 'usernames', 'my_secret_key')
+hashed_df = hash_and_truncate_hmac_swifter(df, 'usernames', 'my_secret_key')
 print(hashed_df)
