a3f2681304782cfee18dcc24613e1760087588e0
Daniel <39141263+uaineteine@users.noreply.github.com>
2024-10-20 01:37:10 +1100

    Update hash_ds.py

--- hash_ds.py
+++ hash_ds.py
@@ -2,14 +2,16 @@ import pandas as pd
 import hmac
 import hashlib
 import swifter
+from multiprocessing import cpu_count
 
 def hmac_and_truncate_value(value, key, length=24):
     hashed_value = hmac.new(key.encode(), str(value).encode(), hashlib.sha256).hexdigest()
     return hashed_value[:length]
 
 def hash_and_truncate_hmac_swifter(df, column, key, length=24):
+    print(f"Using {cpu_count()} threads")
     new_col_name = column + "_hash"
-    df[new_col_name] = df[column].swifter.apply(lambda x: hmac_and_truncate_value(x, key, length))
+    df[new_col_name] = df[column].swifter.set_npartitions(cpu_count()).apply(lambda x: hmac_and_truncate_value(x, key, length))
     return df
 
 # Example usage
