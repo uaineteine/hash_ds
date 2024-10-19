43b2f3e678fe86595e455251d78e517ce1498226
Daniel Stamer-Squair <uaine.teine@hotmail.com>
2024-10-20 09:58:14 +1100

    hashing datasets

--- hash_ds.py
+++ hash_ds.py
@@ -1,21 +1,50 @@
+print("[hash_ds.py] importing libraries...")
 import pandas as pd
 import hmac
 import hashlib
 import swifter
 from multiprocessing import cpu_count
 
+print("[hash_ds.py] defining functions...")
 def hmac_and_truncate_value(value, key, length=24):
     hashed_value = hmac.new(key.encode(), str(value).encode(), hashlib.sha256).hexdigest()
     return hashed_value[:length]
 
 def hash_and_truncate_hmac_swifter(df, column, key, length=24):
-    print(f"Using {cpu_count()} threads")
+    print(f"Using up to {cpu_count()} threads")
     new_col_name = column + "_hash"
     df[new_col_name] = df[column].swifter.set_npartitions(cpu_count()).apply(lambda x: hmac_and_truncate_value(x, key, length))
     return df
 
+def read_file(filepath):
+    format = filepath.split('.')[-1]
+    if format == "csv":
+        return pd.read_csv(filepath)
+    elif format in ("xlsx", "xls"):
+        return pd.read_excel(filepath)
+    elif format == "sas7bdat":
+        return pd.read_sas(filepath)
+    elif format == "psv":
+        return pd.read_csv(filepath, delimiter='|')
+    else:
+        raise ValueError("Unsupported file extension")
+
+print("[hash_ds.py] performing hashing...")
+
+# df = read_file('yourfile.xlsx')
+# df = read_file('yourfile.sas7bdat')
+# df = read_file('yourfile.psv')
+
 # Example usage
-data = {'usernames': ['user1', 'user2', 'user3']}
-df = pd.DataFrame(data)
+#data = {'usernames': ['user1', 'user2', 'user3']}
+#df = pd.DataFrame(data)
+df = read_file('testdata/data2.csv')
 hashed_df = hash_and_truncate_hmac_swifter(df, 'usernames', 'my_secret_key')
 print(hashed_df)
+
+print("hashing second set")
+df = read_file('testdata/data1.psv')
+hashed_df = hash_and_truncate_hmac_swifter(df, 'usernames', 'my_secret_key')
+print(hashed_df)
+
+
