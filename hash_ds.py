7dc43bee66860d15c6901da0836dc1f13005433c
Daniel Stamer-Squair <uaine.teine@hotmail.com>
2024-10-21 20:24:39 +1100

    improve feedback with signposting

--- hash_ds.py
+++ hash_ds.py
@@ -1,6 +1,6 @@
 from libs import *
 
-print("[hash_ds::.py] defining functions...")
+print("[hash_ds::hash_ds.py] defining functions...")
 def hmac_and_truncate_value(value, key, length=24):
     hashed_value = hmac.new(key.encode(), str(value).encode(), hashlib.sha256).hexdigest()
     return hashed_value[:length]
@@ -14,6 +14,7 @@ def hash_and_truncate_hmac_swifter(df, columns, key, length=24):
     return df
 
 def main():
+    print("[hash_ds::hash_ds.py] reading arguments...")
     parser = argparse.ArgumentParser(description="Hash specified columns of a file")
     parser.add_argument('filepath', type=str, help='Path to the file to hash')
     parser.add_argument('columns', type=str, help='Comma-separated list of columns to hash')
@@ -28,8 +29,11 @@ def main():
     length = args.length
     output_loc = args.output_loc
 
+    print("[hash_ds::hash_ds.py] reading data")
     df = read_file(filepath)
+    print("[hash_ds::hash_ds.py] hashing")
     hashed_df = hash_and_truncate_hmac_swifter(df, columns, key, length=length)
+    print("[hash_ds::hash_ds.py] saving data")
     hashed_df.to_parquet(output_loc, index=False)
     #print(hashed_df)
 
